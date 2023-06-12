414b07201fb1b1ca6b64a516925ca0f289d97176
SantiagoBertorello <santinbertorello@gmail.com>
2023-06-12 00:22:53 -0300

    se agrega ejercicio 5 en estructura repetitivas y estructura complejas

---
+++ Ejercicios de estructura repetitivas y estructuras complejas/Ejercicio5.py
@@ -0,0 +1,8 @@
+cantidad_numeros = 1;
+while cantidad_numeros <= 15:
+    numero = int(input(f"ingresa un número negativo:"))
+    resultado = abs(numero)
+    print("El número",numero," en positivo es",resultado)
+    cantidad_numeros += 1;
+
+print("fin del programa");
