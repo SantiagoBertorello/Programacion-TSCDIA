334e97d68350456af4bd194a18f123aca84db029
SantiagoBertorello <santinbertorello@gmail.com>
2023-06-26 02:30:02 -0300

    se carga el archivo ejercicio 3 correctamente

--- Ejercicio Programación estructurada y modular/Ejercicio3.py
+++ Ejercicio Programación estructurada y modular/Ejercicio3.py
@@ -1,4 +1,22 @@
-El resultado de la suma es: 15
-El resultado de la suma es: 15
+import random
 
-***Repl Closed***
+def suma1(parametros):
+    suma = sum(parametros)
+    return suma
+
+parametros = [random.randint(1, 100) for _ in range(3)]
+resultado1 = suma1(parametros)
+print("El resultado es:", resultado1)
+
+
+def suma2(parametros):
+    suma = sum(parametros)
+    return suma
+
+parametros = []
+for i in range(3):
+    numero = int(input("Ingrese el número {}: ".format(i+1)))
+    parametros.append(numero)
+
+resultado2 = suma2(parametros)
+print("El resultado es:", resultado2)
